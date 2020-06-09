d665f81b0e67578423b9b1778a34f95da785501f
Raj761 <64688614+Raj761@users.noreply.github.com>
2020-06-10 03:39:24 +0530

    Add files via upload

--- mail.py
+++ mail.py
@@ -1,8 +1,8 @@
 import smtplib
 rcvr = "rajkumarskotecha@gmail.com"
 sender = "rajkumar.s.kotecha5605@gmail.com"
-msg = "Hey Developer, your model has greater than 96% accuracy and is ready to use for predictions!!" 
-pswd = "12345678" #password for sender's mail address
+msg = "Hey Developer, your model has greater than 95% accuracy and is ready to use for predictions!!" 
+pswd = "@@@@@@" #password for sender's mail address
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
 server.starttls()
